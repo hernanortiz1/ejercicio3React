2260b2b584ed7f6bbd621bfe49bbc17d299e7046
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-11 00:24:03 -0300

    agregue btn y state para cambiar mensaje

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
 const HelloWord = ({ mensajeProps }) => {
-  return (
+const [texto, setTexto] = useState("")
+const mostrarTexto =()=>{
+    setTexto("(from changed state)")
+}
+    return (
     <div>
-      <h1 className="m-3">Hello {mensajeProps}</h1>
+      <h1 className="m-3">Hello {mensajeProps} {texto}</h1>
+      <button className="btn btn-primary m-3" onClick={mostrarTexto}>Click me</button>
     </div>
   );
 };
